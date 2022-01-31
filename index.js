084dac725cb1a25b3ed7e9ad0a71c304b2cadd3d
Georgi Ivanov Georgiev <georgiev7900@gmail.com>
2022-01-31 18:51:12 +0200

    impl edit listing

--- index.js
+++ index.js
@@ -6,38 +6,45 @@
 // [] create data service
 // [] implement controllers
 
-const express = require('express');
-const hbs = require('express-handlebars');
+const express = require("express");
+const hbs = require("express-handlebars");
 
-const carsService = require('./util/cars');
+const carsService = require("./util/cars");
 
-const { home } = require('./controllers/home');
-const { about } = require('./controllers/about');
-const create = require('./controllers/create');
-const { details } = require('./controllers/details');
-const deleteCar = require('./controllers/erase');
-const { notFound404 } = require('./controllers/notFound');
+const { home } = require("./controllers/home");
+const { about } = require("./controllers/about");
+const create = require("./controllers/create");
+const { details } = require("./controllers/details");
+const deleteCar = require("./controllers/erase");
+const edit = require("./controllers/edit");
+
+const { notFound404 } = require("./controllers/notFound");
 
 const app = express();
 
-app.engine('hbs', hbs.create({
-    extname: '.hbs'
-}).engine);
+app.engine(
+  "hbs",
+  hbs.create({
+    extname: ".hbs",
+  }).engine
+);
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
+app.use("/static", express.static("static"));
 app.use(carsService());
 
-app.get('/', home);
-app.get('/about', about);
-app.get('/details/:id', details);
+app.get("/", home);
+app.get("/about", about);
+app.get("/details/:id", details);
 
-app.route('/create').get(create.get).post(create.post);
+app.route("/create").get(create.get).post(create.post);
 
-app.route('/erase/:id').get(deleteCar.get).post(deleteCar.post)
+app.route("/erase/:id").get(deleteCar.get).post(deleteCar.post);
 
-app.all('*', notFound404);
+app.route("/edit/:id").get(edit.get).post(edit.post);
+
+app.all("*", notFound404);
 
 app.listen(3000, () => console.log(`Server started on port 3000`));
--- util/cars.js
+++ util/cars.js
@@ -1,98 +1,111 @@
-const fs = require('fs/promises');
+const fs = require("fs/promises");
 
-const filePath = './util/data.json'
+const filePath = "./util/data.json";
 
 async function read() {
-    try {
-        const file = await fs.readFile(filePath);
-        return JSON.parse(file);
-
-    } catch (err) {
-        console.error(`Database write error`);
-        console.error(err);
-        process.exit(1)
-    }
+  try {
+    const file = await fs.readFile(filePath);
+    return JSON.parse(file);
+  } catch (err) {
+    console.error(`Database write error`);
+    console.error(err);
+    process.exit(1);
+  }
 }
 
 async function write(data) {
-    try {
-        await fs.writeFile(filePath, JSON.stringify(data, null, 2));
-
-    } catch (err) {
-        console.error(`Database read error`);
-        console.error(err);
-        process.exit(1)
-    }
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error(`Database read error`);
+    console.error(err);
+    process.exit(1);
+  }
 }
 
 async function getAll(query) {
-    const data = await read();
-    let cars = Object
-        .entries(data)
-        .map(([id, v]) => Object.assign({}, { id }, v));
+  const data = await read();
+  let cars = Object.entries(data).map(([id, v]) =>
+    Object.assign({}, { id }, v)
+  );
 
-    if (query.search) {
-        cars = cars.filter(car => car.name.toLocaleLowerCase().includes(query.search.toLocaleLowerCase()));
-    };
+  if (query.search) {
+    cars = cars.filter((car) =>
+      car.name.toLocaleLowerCase().includes(query.search.toLocaleLowerCase())
+    );
+  }
 
-    if (query.from) {
-        cars = cars.filter(car => car.price >= Number(query.from))
-    }
+  if (query.from) {
+    cars = cars.filter((car) => car.price >= Number(query.from));
+  }
 
-    if (query.to) {
-        cars = cars.filter(car => car.price <= Number(query.to))
-    }
+  if (query.to) {
+    cars = cars.filter((car) => car.price <= Number(query.to));
+  }
 
-    return cars;
+  return cars;
 }
 
 async function getById(id) {
-    const data = await read();
-    const car = data[id];
+  const data = await read();
+  const car = data[id];
 
-    if (car) {
-        return Object.assign({}, { id }, car);
-    } else {
-        return undefined;
-    }
+  if (car) {
+    return Object.assign({}, { id }, car);
+  } else {
+    return undefined;
+  }
 }
 
 async function createCar(car) {
-    const cars = await read();
+  const cars = await read();
 
-    let id;
+  let id;
 
-    do {
-        id = nexId();
-    } while (cars.hasOwnProperty(id));
+  do {
+    id = nexId();
+  } while (cars.hasOwnProperty(id));
 
-    cars[id] = car;
+  cars[id] = car;
 
-    await write(cars);
+  await write(cars);
 }
 
 async function deleteById(id) {
-    const data = await read();
+  const data = await read();
 
-    if (data.hasOwnProperty(id)) {
-        delete data[id];
-        await write(data);
-    } else {
-        throw new Error(`No such ID in database!`)
-    }
+  if (data.hasOwnProperty(id)) {
+    delete data[id];
+    await write(data);
+  } else {
+    throw new Error(`No such ID in database!`);
+  }
 }
 
+async function updateById(id, car) {
+  const data = await read();
+
+  if (data.hasOwnProperty(id)) {
+    data[id] = car;
+    await write(data);
+  } else {
+    throw new Error(`No such ID in database!`);
+  }
+}
 
 function nexId() {
-    return 'xxxxxxxx-xxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString(16));
+  return "xxxxxxxx-xxxx".replace(/x/g, () =>
+    ((Math.random() * 16) | 0).toString(16)
+  );
 }
 
 module.exports = () => (req, res, next) => {
-    req.storage = {
-        getAll,
-        getById,
-        createCar,
-        deleteById
-    };
-    next();
-}
+  req.storage = {
+    getAll,
+    getById,
+    createCar,
+    deleteById,
+    updateById,
+  };
+  next();
+};
